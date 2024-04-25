a36850c6b853fd0d8859280d3e9ba3fe35cf02ad
daneeskripter <imdanee@seznam.cz>
2024-04-25 21:14:19 +0200

    setup added (not finished)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const { checkNotSetup, checkSetup } = require('../handlers/checkAuth');
+const Settings = require('../models/Settings');
+const User = require('../models/UserModel');
 const router = express.Router();
 const router2 = express.Router()
+const bcrypt = require('bcrypt')
+const axios = require('axios')
 
 router.get("/", checkSetup, function (req, res) {
     res.render("dash.html", {})
@@ -9,6 +13,31 @@ router.get("/", checkSetup, function (req, res) {
 
 router2.get("/", checkNotSetup, function (req, res) {
     res.render("setup.html", {})
+})
+
+router2.post("/", checkNotSetup, async function (req, res) {
+    // BASIC SETUP
+    Settings.create({key: "setup", value: "true", type: "boolean"})
+    Settings.create({key: "hostname", value: req.body.hostname, type: "string"})
+    Settings.create({key: "pterourl", value: req.body.pterourl, type: "string"})
+    Settings.create({key: "pteroapikey", value: req.body.pteroapikey, type: "string"})
+
+    // ADMIN SETUP
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    const user = await axios.get(`${req.body.pterourl}/api/application/users/${req.body.pteroid}`, {
+        headers: { 'Authorization': `Bearer ${req.body.pteroapikey}` }
+    })
+    User.create({username: user.data.attributes.username, password: hashedPassword, email: user.data.attributes.email, admin: true, pteroId: req.body.pteroid, ipAddress: ipAddress})
+    /*await axios.patch(`${req.body.pterourl}/api/application/users/${req.body.pteroid}`, { 
+        headers: { 'Authorization': `Bearer ${req.body.pteroapikey}` },
+        data: [
+            {
+                password: req.body.password
+            }
+        ]
+    })*/ // TODO: FIX PASSWORD UPDATE
+    res.redirect("/auth/login")
 })
 
 
