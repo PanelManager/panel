3bdac5aed627a1195499a883a4a6d0c4e4abc34c
daneeskripter <imdanee@seznam.cz>
2024-04-26 19:05:30 +0200

    added ptero api

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,8 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const passport = require('passport');
 const SettingsModel = require('../models/Settings');
+const axios = require('axios')
+
 
 router.use(checkSetup)
 
@@ -27,20 +29,40 @@ router.get("/register", checkNotAuth, async function (req, res) {
 router.post("/register", checkNotAuth, async function (req, res) {
     const userUsername = await User.findOne({ where: { username: req.body.username}})
     const userEmail = await User.findOne({ where: { email: req.body.email}})
+    let ipCheck = await User.findOne({where: { ipAddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress}})
+    let ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    if ((await SettingsModel.findOne({where: {key: "registerip"}})).value == 'false') {
+        ipCheck = false
+        ipAddress = null
+    }
     if (userUsername) {
         req.flash("registerError", "User already registered with that username")
         res.redirect("/auth/register")
     } else if (userEmail) {
         req.flash("registerError", "User already registered with that email")
         res.redirect("/auth/register")
+    } else if (ipCheck) {
+        req.flash("registerError", "User already registered with that IP")
+        res.redirect("/auth/register")
     } else {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const panelurl = (await SettingsModel.findOne({ where: { key: "pterourl"}})).value
+        const apikey = (await SettingsModel.findOne({ where: { key: "pteroapikey" }})).value
+        const user = await axios.post(`${panelurl}/api/application/users`, {
+            email: req.body.email,
+            username: req.body.username,
+            first_name: req.body.username,
+            last_name: req.body.username,
+            password: req.body.password
+        }, {
+            headers: {'Authorization': `Bearer ${apikey}`}
+        })
         User.create({
             username: req.body.username,
             password: hashedPassword,
             email: req.body.email,
-            pteroId: 0, // TODO: Implement Ptero API
-            ipAddress: "", // TODO: Register IP address
+            pteroId: user.data.attributes.id, 
+            ipAddress: ipAddress,
             credits: 0,
             verifiedEmail: false
         })
