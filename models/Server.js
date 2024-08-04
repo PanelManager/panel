a5f9c24d906852979c925b2b7972e232976414ad
daneeskripter <imdanee@seznam.cz>
2024-08-04 16:46:56 +0200

    server model added

---
+++ models/Server.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+const db = require("../handlers/db");
+
+const ServerModel = db.define("Server", {
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    pteroId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    planId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    eggId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+
+}, {
+    timestamps: false,
+    tableName: "servers",
+})
+ServerModel.sync()
+
+module.exports = ServerModel;
