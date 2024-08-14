afbb348a6688a582f795ad1142d02db048131b0f
daneeskripter <imdanee@seznam.cz>
2024-08-14 14:33:02 +0200

    servers tab added

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ const axios = require("axios")
 const Node = require("../models/Node")
 const Egg = require("../models/Egg")
 const Server = require("../models/Server")
+const Plan = require("../models/Plan")
 
 router.use(checkAdmin)
 
@@ -20,12 +21,30 @@ router.get("/overview", checkAuth, checkAdmin, async function (req, res) {
     const nodesCount = await Node.count()
     const eggsCount = await Egg.count()
     const serversCount = await Server.count()
-    res.render("admin/overview.html", {hostname: (await SettingsModel.findOne({where: {name: "hostname"}})).value, username: req.user.username, gravatarhash: sha256(req.user.email), credits: req.user.credits, pterourl: (await SettingsModel.findOne({where: {name: "pterourl"}})).value, isAdmin: req.user.admin, page: "Overview", users: usersCount, nodes: nodesCount, eggs: eggsCount, servers: serversCount})
+
+    const eggsData = await Egg.findAll()
+    res.render("admin/overview.html", {hostname: (await SettingsModel.findOne({where: {name: "hostname"}})).value, username: req.user.username, gravatarhash: sha256(req.user.email), credits: req.user.credits, pterourl: (await SettingsModel.findOne({where: {name: "pterourl"}})).value, isAdmin: req.user.admin, page: "Overview", users: usersCount, nodes: nodesCount, eggs: eggsCount, servers: serversCount, eggsData: eggsData})
 })
 
 router.get("/users", checkAuth, checkAdmin,  async function (req, res) {
     const users = await UserModel.findAll()
     res.render("admin/users.html", {hostname: (await SettingsModel.findOne({where: {name: "hostname"}})).value, username: req.user.username, gravatarhash: sha256(req.user.email), credits: req.user.credits, pterourl: (await SettingsModel.findOne({where: {name: "pterourl"}})).value, isAdmin: req.user.admin, page: "Users", users: users})
+})
+
+router.get("/servers", checkAuth, checkAdmin, async function (req, res) {
+    const servers = await Server.findAll()
+    const serversArray = await Promise.all(servers.map(async server => {
+        let serverObject = {
+            id: server.id,
+            name: server.name,
+            pteroId: server.pteroId,
+            plan: (await Plan.findOne({where: {id: server.planId}})).name,
+            egg: (await Egg.findOne({where: {id: server.eggId}})).name,
+            owner: (await UserModel.findOne({where: {id: server.ownerId}})).username
+        }
+        return serverObject;
+    }))
+    res.render("admin/servers.html", {hostname: (await SettingsModel.findOne({where: {name: "hostname"}})).value, username: req.user.username, gravatarhash: sha256(req.user.email), credits: req.user.credits, pterourl: (await SettingsModel.findOne({where: {name: "pterourl"}})).value, isAdmin: req.user.admin, page: "Servers", servers: serversArray})
 })
 
 
