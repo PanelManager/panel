6b06b8597e3503d244f298b6a9a1677c3c231026
daneeskripter <imdanee@seznam.cz>
2024-10-31 21:19:28 +0100

    build.sh added

---
+++ build.sh
@@ -0,0 +1,3 @@
+npm install # Install packages
+npx tailwindcss -i ./public/css/input.css -o ./public/css/tailwind.css # Build CSS files
+cp ./node_modules/flowbite/dist/flowbite.min.js ./public/js/flowbite.min.js # Build Flowbite.js files
